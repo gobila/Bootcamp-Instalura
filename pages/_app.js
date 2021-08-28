6683274e7e0739f7da1dd5f83627d5f52904960d
Moises <mcpfla@gmail.com>
2021-08-27 23:19:54 -0300

    feat(seo componet): add seo componet
    add a componet for SEO and de necessary property

--- pages/_app.js
+++ pages/_app.js
@@ -3,16 +3,19 @@ import { ThemeProvider } from 'styled-components';
 import Head from 'next/head';
 import theme from '../src/components/theme';
 import { GlobalStyle } from '../src/components/theme/GlobalStyle';
+import SEO from '../src/components/common/SEO';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title> Instalura - porjeto Base</title>
+        {/* <title> Instalura - porjeto Base</title> */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet" />
       </Head>
+      <SEO headTitle="Home" />
+
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Component {...pageProps} />
