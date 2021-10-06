2b6a3e4c04e92f326ae70d2405da7737c2ffb33e
Moises <mcpfla@gmail.com>
2021-10-05 22:24:13 -0300

    feat: criando a conexão com o dato + AboutPage

--- pages/about.js
+++ pages/about.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import { GraphQLClient, gql } from 'graphql-request';
+import websitePageHOC from '../src/components/wrappers/WebsitePage/hoc';
+import AboutScreen from '../src/components/screens/AboutScreen';
 
-export default function About() {
-  return (
-    <div>
-      Página de Sobre
-
-      <a href="/">
-        Voltar para a home com refresh
-      </a>
-    </div>
-  );
+export async function getStaticProps() {
+  const TOKEN = 'e1120989073ca84cfe633389f3d8b7';
+  const DataCMSURL = 'https://graphql.datocms.com/';
+  const client = new GraphQLClient(DataCMSURL, {
+    headers: {
+      Authorization: `Bearer ${TOKEN}`,
+    },
+  });
+  const query = gql`
+    query{
+      pageSobre {
+        pageTitle
+        pageDescription
+      }
+    }
+  `;
+  const messages = await client.request(query);
+  return {
+    props: {
+      messages,
+    },
+  };
 }
+
+export default websitePageHOC(AboutScreen);
